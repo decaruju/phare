8d7795f227094bdbef29b0c7133b65f252cec2e2
Julien de Carufel <julien.decaruf@gmail.com>
2018-02-03 22:05:03 +0000

    install resources

--- phare/phare/settings.py
+++ phare/phare/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['192.168.108.143', '127.0.0.1']
 
 INSTALLED_APPS = [
     'flux',
+    'resources',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
