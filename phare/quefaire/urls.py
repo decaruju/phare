ab883694cf6aa5606b17e9e65c023e99c6e6dd6c
Alexandre Ouellet <alexandre.ouellet67@hotmail.com>
2018-02-04 09:03:06 -0500

    Page que faire et side mod

--- phare/quefaire/urls.py
+++ phare/quefaire/urls.py
@@ -5,5 +5,6 @@ from . import views
 from flux.models import Message
 
 urlpatterns = [
-	path('', views.que_faire),
+	path('', views.que_faire, name='quefaire'),
+	path('<str:sujet>', views.que_faire_detail, name='quefaire_detail')
 ]
