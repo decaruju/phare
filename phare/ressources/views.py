5f8e64b0f1c5b987d84dc8e4b6f4974a38d2d218
Alexandre Ouellet <alexandre.ouellet67@hotmail.com>
2018-02-03 20:44:45 -0500

    Mise à jour des liens vers les fichiers

--- phare/ressources/views.py
+++ phare/ressources/views.py
@@ -9,9 +9,9 @@ def choisir_ressource(request):
     ressources = Ressource.objects.all()
     context = RequestContext(request)
     context_dict = {'ressources': ressources, 'langue': request.LANGUAGE_CODE}
-    return render_to_response("choix_ressource.html.j2", context_dict, context)
+    return render_to_response("choix_ressource.html", context_dict, context)
 
 
 def detail_ressource(request):
 
-	return render_to_response(request, "choix_ressource.html.j2")
+	return render_to_response(request, "choix_ressource.html")
