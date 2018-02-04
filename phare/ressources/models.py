70f04dd2e5df94946328bff1d6a2c0724390240b
Charles Sirois <sirois.charles@protonmail.com>
2018-02-03 19:50:45 -0500

    i18n pr ressources

--- phare/ressources/models.py
+++ phare/ressources/models.py
@@ -4,7 +4,9 @@ from django.db import models
 class Ressource(models.Model):
     nom = models.CharField(max_length=20)
     adresse = models.TextField()
-    description = models.TextField()
+    description_fr = models.TextField()
+    description_en = models.TextField()
+
     a_hebergement = models.BooleanField()
     a_soins = models.BooleanField()
     a_biens_enssentiels = models.BooleanField()
--- phare/ressources/views.py
+++ phare/ressources/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response, render
+from django.template import RequestContext
+from django.http import HttpResponse
+from ressources.models import Ressource
 
 # Create your views here.
 
 def choisir_ressource(request):
-	return render(request, "choix_ressource.html.j2")
+    ressources = Ressource.objects.all()
+    context = RequestContext(request)
+    context_dict = {'ressources': ressources, 'langue': request.LANGUAGE_CODE}
+    return render_to_response("choix_ressource.html.j2", context_dict, context)
