6d35d50df9f7fddd826304a1c40c5f24ca176a13
Julien de Carufel <julien.decaruf@gmail.com>
2018-02-04 00:07:41 +0000

    Je sais pas trop

--- phare/phare/settings.py
+++ phare/phare/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['192.168.108.143', '127.0.0.1']
 INSTALLED_APPS = [
     'flux',
     'resources',
+    'autorite',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
